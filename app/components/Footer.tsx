af6643d92c6b102f2362ec00c9e5355df2e1f276
Otavio Barbosa <otavioblbarbosa@gmail.com>
2025-02-07 12:20:18 -0300

    Added footer and header components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-6 mb-8'>
+      <p className='text-center'>Made by Otavio Barbosa</p>
+    </footer>
+
+  )
+}
+
+export default Footer
